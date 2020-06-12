b5ed7232721728cb04af0fc377e24c2cf3499edd
Giancarlos Salas <giansalex@gmail.com>
2020-06-11 23:38:53 -0500

    Remove dynamic properties

--- crypto/order.go
+++ crypto/order.go
@@ -2,13 +2,10 @@ package crypto
 
 // Order order parameters
 type Order struct {
-	APIKey string  `json:"api_key"`
 	Side   string  `json:"side"`
 	Symbol string  `json:"symbol"`
-	Time   int     `json:"time"`
 	Type   int     `json:"type"`
 	Volume float64 `json:"volume"`
-	Sign   string  `json:"sign"`
 }
 
 type orderResponse struct {
