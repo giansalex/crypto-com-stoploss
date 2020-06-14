d95c6e6d709027bd29e6d69ebd130e37ebab4723
Giancarlos Salas <giansalex@gmail.com>
2020-06-13 21:55:10 -0500

    Notify to telegram chat

--- stoploss/notify.go
+++ stoploss/notify.go
@@ -1,14 +1,33 @@
 package stoploss
 
-import "fmt"
+import (
+	"fmt"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
 
 // Notify notify stoploss
 type Notify struct {
 	tlgToken string
-	chanelID int
+	chanelID int64
 }
 
 // Send send message
 func (notify *Notify) Send(message string) {
 	fmt.Println(message)
+
+	if notify.tlgToken == "" {
+		return
+	}
+
+	bot, err := tgbotapi.NewBotAPI(notify.tlgToken)
+	if err != nil {
+		fmt.Println("Cannot connect to Telegram:", err.Error())
+
+		return
+	}
+
+	msg := tgbotapi.NewMessage(notify.chanelID, message)
+
+	bot.Send(msg)
 }
